f72afb3baf66876a71b83a6a6e5f5821194b7f81
hugolfsimoes <hugolfsimoes@gmail.com>
2022-03-15 19:09:06 -0300

    feat(visao-geral): implementado os cards na page visao geral

--- src/pages/VisaoGeral/index.js
+++ src/pages/VisaoGeral/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Card from '../../components/Card';
 
 export default function VisaoGeral() {
   return (
-    <div>Visao Geral</div>
+    <div>
+      <h2 className='title-page'>Visão Geral</h2>
+      <div className='card-container'>
+        <Card title="Total de Lojas" result="7213" />
+        <Card title="Faturamento total" result="100.000,00" pink />
+        <Card title="Loja destaque" result="Estilo Pri" />
+        <Card title="Meta Mensal" result="110.000,00" />
+      </div>
+    </div>
   );
 }
