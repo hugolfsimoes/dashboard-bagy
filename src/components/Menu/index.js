7d797bc5c6a8edd65c5139beb566e15f7a57ef24
hugolfsimoes <hugolfsimoes@gmail.com>
2022-03-15 17:11:29 -0300

    feat(menu): criado os links do menu com as rotas das paginas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 export default function Menu() {
-  const menuItens = ['Visão Geral', 'Lojas', 'Vendas', 'Clientes', 'Produtos', 'Planos e Metas', 'Configurações', 'Sair'];
+  const menuItens = ['', '', '', '', '', '', ''];
   return (
     <aside className='Menu'>
       <nav>
         <ul>
-          {menuItens.map(item => (
-            <li key={item}>
-              <a href="/">{item}</a>
-            </li>
-          ))}
+          <li>
+            <Link to="/">Visão Geral</Link>
+          </li>
+          <li>
+            <Link to="/lojas">Lojas</Link>
+          </li>
+          <li>
+            <Link to="/vendas">Vendas</Link>
+          </li>
+          <li>
+            <Link to="/clientes">Clientes</Link>
+          </li>
+          <li>
+            <Link to="/produtos">Produtos</Link>
+          </li>
+          <li>
+            <Link to="/planos-metas">Planos e Metas</Link>
+          </li>
+          <li>
+            <Link to="/configuracoes">Configurações</Link>
+          </li>
+          <li>
+            <Link to="/logout">Sair</Link>
+          </li>
+
         </ul>
       </nav>
     </aside>
